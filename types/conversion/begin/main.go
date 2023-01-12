695bb775e25a21e67484a3772db0d4879bbb6195
Jaime Escobar <jaimescoseru@gmail.com>
2023-01-12 02:12:58 +0000

    conversions

--- types/conversion/begin/main.go
+++ types/conversion/begin/main.go
@@ -1,6 +1,13 @@
 // types/conversion/begin/main.go
 package main
 
+import "fmt"
+
+// Go will create a new allocation for a converted value
 func main() {
 	// declare float and convert it to an unsigned int
+	a := 42.5
+	b := uint(a)
+
+	fmt.Printf("%v=%T, %v=%T\n", a, a, b, b)
 }
